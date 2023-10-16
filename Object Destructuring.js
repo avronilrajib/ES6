d3d4cbf048e7dd29ca49b41edb02f710fbb26341
avronil rajib <110673121+avronilrajib@users.noreply.github.com>
2023-10-17 01:27:49 +0600

    Object Distructuring

---
+++ Object Destructuring.js
@@ -0,0 +1,14 @@
+//ES6 Class Number 9
+//Object Destructuring
+
+const person={
+    name:"Rajib Dash",
+    age:21,
+    id:"221-115-075",
+    blood:"A+",
+}
+const {name,age,id,blood} = person;
+console.log(name);
+console.log(age);
+console.log(id);
+console.log(blood);
