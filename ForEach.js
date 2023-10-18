eb8cde8b08a148eafa99beb222a678df05191df3
avronil rajib <110673121+avronilrajib@users.noreply.github.com>
2023-10-18 22:56:10 +0600

    ForEach

---
+++ ForEach.js
@@ -0,0 +1,14 @@
+const num=[1,2,3,4,5,6,7,8];
+const personName=["Rajib","Avronil","Sajib","Sagor"];
+let nameF=(name)=>{
+    console.log(name);
+  }
+personName.forEach(nameF);
+
+num.forEach(Fun);
+function Fun(value){
+    console.log(value);
+}
+
+let oddNum=[1,3,5,7,9,11];
+oddNum.forEach(x=> console.log(x));
