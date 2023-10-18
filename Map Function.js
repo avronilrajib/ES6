f193b5ea28657b98285b9250da72fa9feb3aa874
avronil rajib <110673121+avronilrajib@users.noreply.github.com>
2023-10-18 23:19:15 +0600

    Map Function

---
+++ Map Function.js
@@ -0,0 +1,15 @@
+let num=[2,4,6,8,10];
+let sqrt=num.map(squert);
+console.log(num);
+function squert(x){
+    return x*2;
+}
+console.log(sqrt);
+
+const personName=["Rajib","Avronil","Sajib","Sagor"];
+let pnt=personName.map(output);
+
+function output(x){
+    return x;
+}
+console.log(pnt);
